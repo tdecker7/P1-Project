6540bf923edb23b9b7cefbc5291df4b6919f9ddb
Trey Decker <treydecker@Treys-MacBook-Air.local>
2018-07-03 11:25:31 -0600

    able to select DOM elements via child components

--- src/app/sort-list/sort-list.component.ts
+++ src/app/sort-list/sort-list.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChildren } from '@angular/core';
+
+import { SortItemComponent } from '../sort-item/sort-item.component';
 
 @Component({
   selector: 'app-sort-list',
   templateUrl: './sort-list.component.html',
   styleUrls: ['./sort-list.component.css']
 })
-export class SortListComponent implements OnInit {
+export class SortListComponent implements AfterViewInit {
   sortItems: Array<number> = [];
+  @ViewChildren(SortItemComponent) childItems: SortItemComponent[];
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.fillArray(this.sortItems, 10);
-    console.log(this.sortItems);
+    console.log(this.childItems);
   }
 
   fillArray(array, length): void {
     for (let i = 0; i < length; i++) {
       array.push(i);
     }
+  }
+
+  sort(): void {
+    this.childItems.forEach(item => {
+      console.log(item.item);
+    });
   }
 }
