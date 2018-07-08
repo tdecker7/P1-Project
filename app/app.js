cf17666fbc9ec4f7f2a3ab67be6af8b02ed79729
Trey Decker <treydecker@Treys-MacBook-Air.local>
2018-07-08 12:48:16 -0600

    functional swap and selection sort

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ function buildSortItems(parent, array) {
 function sort() {
     const sortListElements = document.getElementById('sort-list').childNodes;
     switch (SELECTEDSORT.selected) {
-        case 'merge': {
+        case 'bubble': {
             bubbleSort(sortListElements);
             break;
         }
@@ -49,9 +49,50 @@ function getSelectedSort() {
 
 }
 function swapElementsRebuild(elem1, elem2) {
-    // Build function that can swap any two elements. 
-    // not just elements side-by-side
+    const elemRect1 = elem1.getBoundingClientRect();
+    const elemRect2 = elem2.getBoundingClientRect();
+    const elemOffset1 = elemRect2.left - elemRect1.left;
+    const elemOffset2 = elemRect1.left - elemRect2.left;
+    // const origElem1 = elemRect1.left;
+    // const origElem2 = elemRect2.left;
+
+    let elem2Clone = elem2.cloneNode(true);
+    elem2Clone.style.display = 'hidden';
+    elem2.parentNode.insertBefore(elem2Clone, elem2);
+    elem1.parentNode.insertBefore(elem2, elem1);
+    elem2Clone.parentNode.insertBefore(elem1, elem2Clone);
+    elem2Clone.parentNode.removeChild(elem2Clone);
+
+    elem2.style.left = elemOffset1;
+    elem1.style.left = elemOffset2;
+    let inc1 = elemOffset2;
+    let inc2 = elemOffset1;
+    const incrementer = Math.abs( (elemRect1.left - elemRect2.left) / 100 ); // set interval should run 100 times to take up a second.
+    const interval1 = setInterval(moveOne, 10);
+    const interval2 = setInterval(moveTwo, 10);
+
+    function moveOne() {
+        let left = parseFloat(elem1.style.left, 10);
+        if (left >= 0) {
+            clearInterval(interval1);
+            elem1.style.left = 0;
+        } else {
+            inc1 += incrementer;
+            elem1.style.left = `${inc1}px`;
+        }
+    }
+    function moveTwo() {
+        let left = parseFloat(elem2.style.left, 10);
+        if (left <= 0) {
+            clearInterval(interval2);
+            elem2.style.left = 0;
+        } else {
+            inc2 -= incrementer;
+            elem2.style.left = `${inc2}px`;
+        }
+    }
 }
+
 function swapElements(elem1, elem2) {
     elem2.classList.add('animated','slideInRight');
     elem1.classList.add('animated', 'slideInLeft');
@@ -76,7 +117,25 @@ async function bubbleSort(array) {
     }
 }
 async function selectionSort(array) {
-    swapElementsRebuild(array[0], array[4]);
+    let length = array.length;
+    let min;
+
+    for (i = 0; i < length; i++) {
+        min = i;
+        
+        for (j = i + 1; j < length; j++) {
+            let num1 = Number(array[j].innerText);
+            let num2 = Number(array[min].innerText);
+            if (num1 < num2) {
+                min = j;
+            }
+        }
+
+        if (i != min) {
+            swapElementsRebuild(array[i], array[min]);
+            await sleep(1000);
+        }
+    }
 }
 function sleep(milliseconds) {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
